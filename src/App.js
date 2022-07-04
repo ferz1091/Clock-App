1e1fb9fd98b82a65b65d539052c2ae00dcb5ad67
ferz1091 <ferz1091@gmail.com>
2022-07-04 21:05:07 +0300

    encapsulation styled App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 // Core
 import { Routes, Route } from 'react-router-dom';
+import styled from 'styled-components';
 
 // Components
 import { Navbar, Clock, Timer, Weather } from './view/components';
 
-// Styles
-import { AppContainer } from './styles';
+const AppContainer = styled.div`
+font-size: 25px;
+position: relative;
+width: 100vw;
+height: 100vh;
+background: linear-gradient(to top, rgb(60, 60, 60) 0%,  rgb(30, 30, 30) 50%,  rgb(60, 60, 60) 100%);
+`;
 
 const App = () => {
   return (
